89a70a2ec1388763a5a81ad3650657aa8727ee11
upup2544 <upup2544@hotmail.com>
2022-05-03 17:04:05 +0700

    vuelidate

--- client/src/views/CreateCustomer.vue
+++ client/src/views/CreateCustomer.vue
@@ -29,10 +29,10 @@
 <template>
   <form @submit.prevent="someAction()">
     <div class="field">
-      <label for="passport_data"> ชุดหนังสือเดินทางและหมายเลข </label>
-      <input id="passport_data" type="text" v-model="contactNumber" />
-      <div class="error" v-if="$v.contactNumber.$invalid">
-        ซีรี่ส์และหมายเลขหนังสือเดินทางต้องอยู่ในรูปแบบ 081-xxx-xxxx
+      <label for="companyNumber"> เบอร์บริษัท</label>
+      <input id="companyNumber" type="text" v-model="companyNumber" />
+      <div class="error" v-if="$v.companyNumber.$invalid">
+        เบอร์บริษัทต้องอยู่ในรูปแบบ 081-xxx-xxxx
       </div>
     </div>
 
@@ -50,12 +50,39 @@
     </div> -->
 
     <div class="field">
-      <label for="contactName"> ชื่อ</label>
+      <label for="companyName"> ชื่อบริษัท</label>
+      <input id="companyName" type="text" v-model="companyName" @blur="$v.companyName.$touch()" />
+      <div class="error" v-if="$v.companyName.$error">
+        <template v-if="!$v.companyName.$invalid">
+        </template>
+        <template v-else> ต้องระบุชื่อ </template>
+      </div>
+    </div>
+    <div class="field">
+      <label for="contactNumber"> เบอร์ผู้ติดต่อ</label>
+      <input id="contactNumber" type="text" v-model="contactNumber" @blur="$v.contactNumber.$touch()" />
+      <div class="error" v-if="$v.contactNumber.$error">
+        <template v-if="!$v.contactNumber.$invalid">
+        </template>
+        <template v-else> เบอร์บริษัทต้องอยู่ในรูปแบบ 081-xxx-xxxx </template>
+      </div>
+    </div>
+    <div class="field">
+      <label for="contactName"> ชื่อผู้ติดต่อ</label>
       <input id="contactName" type="text" v-model="contactName" @blur="$v.contactName.$touch()" />
       <div class="error" v-if="$v.contactName.$error">
         <template v-if="!$v.contactName.$invalid">
         </template>
         <template v-else> ต้องระบุชื่อ </template>
+      </div>
+    </div>
+    <div class="field">
+      <label for="taxNumber">เลขกำกับภาษี</label>
+      <input id="taxNumber" type="text" v-model="taxNumber" @blur="$v.taxNumber.$touch()" />
+      <div class="error" v-if="$v.taxNumber.$error">
+        <template v-if="!$v.taxNumber.$invalid">
+        </template>
+        <template v-else> หมายเลขกำกับภาษีต้องมี13ตัว</template>
       </div>
     </div>
 
@@ -86,15 +113,26 @@ export default {
   },
 
   validations: {
+     companyNumber: {
+      required,
+      validFormat: (val) => /^\d{3}-\d{3}-\d{4}$/.test(val),
+    },
     contactNumber: {
       required,
       validFormat: (val) => /^\d{3}-\d{3}-\d{4}$/.test(val),
+    },
+    taxNumber: {
+      required,
+      validFormat: (val) => /^\d{13}$/.test(val),
     },
     // passportDate: {
     //   required,
     //   validDate: (val) => moment(val, "DD.MM.YYYY", true).isValid(),
     // },
     contactName: {
+      required,
+    },
+    companyName: {
       required,
     },
   },
