f76b5fb6f5a7ec2bc603de2a91d0df8e0373a6cf
upup2544 <upup2544@hotmail.com>
2022-05-03 15:26:30 +0700

    update server

--- server/routes/quotationRoute.js
+++ server/routes/quotationRoute.js
@@ -5,24 +5,65 @@ import db from "../config/database.js";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const sqlQuotation = "SELECT summernote FROM quotation ";
-    
+    const sqlQuotation = "SELECT * FROM quotation ";
+
     db.query(sqlQuotation, (err, sqlQuotationRe) => {
-        if(err) throw err;
+        if (err) throw err;
         // console.log(sqlQuotationRe);
         res.send(sqlQuotationRe);
     })
 })
 
 router.post('/', (req, res) => {
-    const data = req.body.summernote;
-    const sqlQuotation = "INSERT INTO quotation SET summernote=?";
-    
-    db.query(sqlQuotation,[data], (err, sqlQuotationRe) => {
-        if(err) throw err;
-        // console.log(sqlQuotationRe);
-        res.send(sqlQuotationRe);
-    })
+    const qID = req.body.qID;
+    const cID = req.body.cID;
+    const eID = req.body.eID;
+    const bID = req.body.bID;
+    const statusq = 'Negotiation';
+    const date = req.body.date;
+    const noteq = req.body.noteq;
+    const statusw = 'On-Going';
+    const qIN = req.body.qIN;
+    const vat = req.body.vat;
+    const total = req.body.total;
+    const payment = req.body.payment;
+    const address = req.body.address;
+    const subd = req.body.subd;
+    const d = req.body.d;
+    const prov = req.body.prov;
+    const postcode = req.body.postcode;
+    const taxNumber = req.body.taxNumber;
+    const stamp = req.body.stamp;
+    const companyName = req.body.companyName;
+    const estatus = req.body.estatus;
+    const summernote = req.body.summernote;
+    const sqlQuotation = "INSERT INTO `quotation`(`quotationID`, `customerID`, `employeeID`, `bankID`, `status-quotation`, `date-quotation`, `note-quotation`, `status-work`, `quantity-installment`, `vat`, `address`, `sub-district`, `district`, `province`, `postcode`, `taxNumber`, `stamp`, `companyName`, `paymentPrice`, `summernote`, `e-status`, `total`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    db.query(sqlQuotation, [qID,
+        cID,
+        eID,
+        bID,
+        statusq,
+        date,
+        noteq,
+        statusw,
+        qIN,
+        vat,
+        total,
+        payment,
+        address,
+        subd,
+        d,
+        prov,
+        postcode,
+        taxNumber,
+        stamp,
+        companyName,
+        estatus,
+        summernote], (err, sqlQuotationRe) => {
+            if (err) throw err;
+            // console.log(sqlQuotationRe);
+            res.send(sqlQuotationRe);
+        })
 })
 // router.get('/:id', (req, res) => {
 //     const qID = req.params.id;
