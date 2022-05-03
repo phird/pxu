115a7d677a47ecd8965caec1120743cb9db93894
phird <iamphird@gmail.com>
2022-05-03 14:36:41 +0700

    !important css CreateQuotation

--- client/src/views/CreateQuotation.vue
+++ client/src/views/CreateQuotation.vue
@@ -9,20 +9,12 @@
     </tab-content>
 
 
-
-
-
     <tab-content title="ข้อมูลเพิ่มเติม*">
       <div class="form-group">
         <summernote 
         :sumnote="sumnote" @update-text="update"/>
       </div>
     </tab-content>
-
-
-
-
-
 
 
     <tab-content title="เพิ่มขอบเขตงาน">
@@ -79,66 +71,121 @@ export default {
 
 <style scoped>
 
-.step-progress {
-    display: none;
-    height: 1rem;
-    background: #fff;
-    border-radius: 1rem;
-    margin: 1rem 0;
+.progressbar{
+    -webkit-transition:width 1s ease;
+    transition:width 1s ease}
+.vue-step-wizard{
+    
+    /* background-color:#f7f8fc; */
+    background-color:transparent!important;
+    width:100%!important;
+    margin:auto;
+    padding:40px;
+    padding-top: 0px;
 }
 
-.form-input {
-  border: 1px solid #333;
-  border-radius: 50px;
+.step-header{
+    justify-content: space-between;
 }
-.form-control:focus {
-  box-shadow: none;
-  /* border: none; */
+.step-progress{
+    display: none!important;
+    height:1rem;
+    /* background:#fff; */
+    border-radius:1rem;
+    margin:1rem 0
+} 
+.bar{
+    content:"";
+    height:1rem;
+    border-radius:1rem;
+    background-color:#4b8aeb}
+.step-pills{
+    display:-webkit-box;
+    display:-ms-flexbox;
+    display:flex;
+    /* background-color:#fff; */
+    -webkit-box-pack:justify;
+    -ms-flex-pack:justify;
+    /* justify-content:space-between; */
+    padding:1rem;
+    border-radius:1rem;
+    /* -webkit-box-shadow:0 .5rem 1rem rgba(0,0,0,.15)!important;box-shadow:0 .5rem 1rem rgba(0,0,0,.15)!important */
 }
-.submit-btn {
-  background-color: skyblue;
-  border-radius: 50%;
-  border: none;
-  font-size: 20px;
-  font-weight: 800;
-  color: #333;
+.step-pills .step-item{
+    background-color:transparent!important;
+    border-radius:10px;
+    padding:5px 20px;
+    list-style-type:none;
+    padding:.5rem 1.5rem
+    }
+.step-pills .step-item a{
+    text-decoration:none;
+    color:#7b7b7b
+    }
+.step-pills .step-item.active{
+    /* border:1px solid #4b8aeb */
 }
-.todo-list {
-  border-radius: 50px;
+.step-pills .step-item.active a{
+    color: black;
+    font-weight: bolder;
 }
-.todo-finished {
-  font-style: italic;
-  text-decoration: line-through;
+.step-pills .step-item.validated{
+    border:1px solid #008011
 }
-.quantity-indicator {
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
+.step-body{
+    /* background-color:#fff; */
+    margin-left:auto;
+    -webkit-box-shadow:0 .5rem 1rem rgba(0,0,0,.15)!important;
+    box-shadow:0 .5rem 1rem rgba(0,0,0,.15)!important
 }
-.quantity-indicator-todo {
-  background: red;
+.step-body,.step-footer{
+    padding:1rem;
+    border-radius:1rem
 }
-.quantity-indicator-ongoing {
-  background: yellow;
+.step-footer{
+    margin-left:auto;
+    margin:1rem 0;
+    text-align:center
 }
-.quantity-indicator-finished {
-  background: green;
+.step-button{
+        font-weight:700;
+        line-height:1;
+        text-transform:uppercase;
+        position:relative;
+        max-width:30rem;
+        text-align:center;
+        border:1px solid;
+        border-radius:12px !important;
+        color:#22292f;
+        color:rgba(34,41,47,var(--text-opacity));
+        padding:.5rem 1.25rem;
+        font-size:.875rem;
+        margin:.5rem;
+        color:#fff;
+        outline:none!important;
+        -webkit-box-shadow:none!important;
+        box-shadow:none!important}
+.step-button-next{
+    background-color:#126fde
 }
-.quantity-text {
-  font-weight: bold;
-  cursor: pointer;
+.step-button-previous{
+    background-color:#3deaba
 }
-.quantity-text-todo {
-  color: red;
+.step-button-submit{
+    background-color:#4fa203
 }
-.quantity-text-ongoing {
-  color: yellow;
+.step-button-reset{
+    background-color:#037da2
 }
-.quantity-text-finished {
-  color: green;
-}
-.action-btn i {
-  font-size: 25px;
-  cursor: pointer;
-}
+.tabStatus{
+    display:inline-block;
+    width:1.5rem;
+    height:1.5rem;
+    margin-right:.5rem;
+    line-height:1.5rem;
+    color:#fff;
+    text-align:center;
+    background:rgba(0,0,0,.38);
+    border-radius: 10px;
+    }
 </style>
