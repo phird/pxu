06af44efe91aac475670eba7c710a739b5aa4101
phird <iamphird@gmail.com>
2022-05-24 17:21:53 +0700

    database

--- server/config/database.js
+++ server/config/database.js
@@ -2,9 +2,9 @@ import mysql from "mysql";
  
 // create the connection to database
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'internproject1'
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b0b7de2d8031df',
+  password: '2132ab2a',
+  database: 'heroku_b93f784b1cb597b'
 });
 export default db;
